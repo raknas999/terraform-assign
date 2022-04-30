74ac866a03e8473b2ed246badc689d3d0cb98a97
raknas999 <32408560+raknas999@users.noreply.github.com>
2022-04-30 19:09:03 +0530

    variables file

---
+++ variables.tf
@@ -0,0 +1,89 @@
+# variables.tf | Auth and Application variables
+
+variable "aws_access_key" {
+  type        = string
+  description = "AWS Access Key"
+}
+
+variable "aws_secret_key" {
+  type        = string
+  description = "AWS Secret Key"
+}
+
+variable "aws_key_pair_name" {
+  type        = string
+  description = "AWS Key Pair Name"
+}
+
+variable "aws_region" {
+  type        = string
+  description = "AWS Region"
+}
+
+variable "aws_cloudwatch_retention_in_days" {
+  type        = number
+  description = "AWS CloudWatch Logs Retention in Days"
+  default     = 1
+}
+
+variable "app_name" {
+  type        = string
+  description = "Application Name"
+}
+
+variable "app_environment" {
+  type        = string
+  description = "Application Environment"
+}
+
+variable "cidr" {
+  description = "The CIDR block for the VPC."
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+  description = "List of public subnets"
+}
+
+variable "private_subnets" {
+  description = "List of private subnets"
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+}
+
+variable "app_port" {
+  default     = "5000"
+  description = "portexposed on the docker image"
+}
+
+variable "fargate_cpu" {
+  default     = "1024"
+  description = "fargate instacne CPU units to provision,my requirent 1 vcpu so gave 1024"
+}
+
+variable "fargate_memory" {
+  default     = "2048"
+  description = "Fargate instance memory to provision (in MiB) not MB"
+}
+
+variable "tag" {
+  default     = "latest"
+  description = "docker image tag to pull into ECS cluster"
+}
+
+
+variable "alb_protocol" {
+  default     = "HTTP"
+  description = "Protocol for ALB Target and Listener"
+}
+
+
+variable "svc_count" {
+  default     = "1"
+  description = "Service desire count"
+}
+
+variable "ecr_url" {
+  description = "ECR Repo connectivity URL"
