09720cf9123fee864d8a925cf8590cfa6f245c64
raknas999 <32408560+raknas999@users.noreply.github.com>
2022-04-30 19:46:32 +0530

    Added comments to IAM config file

--- iam.tf
+++ iam.tf
@@ -1,5 +1,6 @@
 # iam.tf | IAM Role Policies
 
+# ECS task execution role
 resource "aws_iam_role" "ecsTaskExecutionRole" {
   name               = "${var.app_name}-execution-task-role"
   assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
@@ -9,6 +10,7 @@ resource "aws_iam_role" "ecsTaskExecutionRole" {
   }
 }
 
+#ECS policy
 data "aws_iam_policy_document" "assume_role_policy" {
   statement {
     actions = ["sts:AssumeRole"]
@@ -20,6 +22,7 @@ data "aws_iam_policy_document" "assume_role_policy" {
   }
 }
 
+# ECS task execution role policy attachment
 resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
   role       = aws_iam_role.ecsTaskExecutionRole.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
