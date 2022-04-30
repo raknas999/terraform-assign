cff3fefd61e057dba421d62eef27bd1645cfc9fa
raknas999 <32408560+raknas999@users.noreply.github.com>
2022-04-30 19:23:53 +0530

    ecs cluster config file

---
+++ ecs.tf
@@ -0,0 +1,79 @@
+# ecs.tf | Elastic Container Service Cluster and Tasks Configuration
+
+resource "aws_ecs_cluster" "aws-ecs-cluster" {
+  name = "${var.app_name}-${var.app_environment}-cluster"
+  tags = {
+    Name        = "${var.app_name}-ecs"
+    Environment = var.app_environment
+  }
+}
+
+resource "aws_cloudwatch_log_group" "log-group" {
+  name = "${var.app_name}-${var.app_environment}-logs"
+
+  tags = {
+    Application = var.app_name
+    Environment = var.app_environment
+  }
+}
+
+data "template_file" "env_vars" {
+  template = file("./templates/env_vars.json")
+  vars = {
+    app_name    = var.app_name
+    app_environment = var.app_environment
+    ecr_url     = var.ecr_url
+    tag         = var.tag
+    fargate_cpu = var.fargate_cpu
+    fargate_memory = var.fargate_memory
+    app_port    = var.app_port
+
+  }
+}
+
+resource "aws_ecs_task_definition" "aws-ecs-task" {
+  family = "${var.app_name}-task"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  memory                   = "${var.fargate_memory}"
+  cpu                      = "${var.fargate_cpu}"
+  execution_role_arn       = aws_iam_role.ecsTaskExecutionRole.arn
+  task_role_arn            = aws_iam_role.ecsTaskExecutionRole.arn
+  container_definitions    = data.template_file.env_vars.rendered
+
+  tags = {
+    Name        = "${var.app_name}-ecs-td"
+    Environment = var.app_environment
+  }
+}
+
+data "aws_ecs_task_definition" "main" {
+  task_definition = aws_ecs_task_definition.aws-ecs-task.family
+}
+
+resource "aws_ecs_service" "aws-ecs-service" {
+  name                 = "flask"
+  cluster              = aws_ecs_cluster.aws-ecs-cluster.id
+  task_definition      = aws_ecs_task_definition.aws-ecs-task.arn
+  launch_type          = "FARGATE"
+  scheduling_strategy  = "REPLICA"
+  desired_count        = "${var.svc_count}"
+  force_new_deployment = true
+
+  network_configuration {
+    subnets          = aws_subnet.public.*.id
+    assign_public_ip = true
+    security_groups = [
+      aws_security_group.service_security_group.id,
+      aws_security_group.load_balancer_security_group.id
+    ]
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.target_group.arn
+    container_name   = "${var.app_name}-${var.app_environment}-container"
+    container_port   = "${var.app_port}"
+  }
+
+  depends_on = [aws_lb_listener.listener]
+}
